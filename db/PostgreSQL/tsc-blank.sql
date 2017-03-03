799c584f9aaf8858f4944fa0d0f822a036c16636
binodnp@outlook.com <binodnp@outlook.com>
2017-03-03 23:37:56 +0545

    -Refactor and Routine Development.

--- db/PostgreSQL/tsc-blank.sql
+++ db/PostgreSQL/tsc-blank.sql
@@ -3,31 +3,31 @@ DELETE FROM auth.menu_access_policy
 WHERE menu_id IN
 (
  SELECT menu_id FROM core.menus
- WHERE app_name = 'tsc'
+ WHERE app_name = 'TheSmsCentral'
 );
 
 DELETE FROM auth.group_menu_access_policy
 WHERE menu_id IN
 (
  SELECT menu_id FROM core.menus
- WHERE app_name = 'tsc'
+ WHERE app_name = 'TheSmsCentral'
 );
 
 DELETE FROM core.menus
-WHERE app_name = 'tsc';
+WHERE app_name = 'TheSmsCentral';
 
 
-SELECT * FROM core.create_app('tsc', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/tsc', NULL);
+SELECT * FROM core.create_app('TheSmsCentral', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/TheSmsCentral', NULL);
 
-SELECT * FROM core.create_menu('tsc', 'Tasks', 'Tasks', '', 'lightning', '');
-SELECT * FROM core.create_menu('tsc', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/tsc', 'configure', 'Tasks');
+SELECT * FROM core.create_menu('TheSmsCentral', 'Tasks', 'Tasks', '', 'lightning', '');
+SELECT * FROM core.create_menu('TheSmsCentral', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/TheSmsCentral', 'configure', 'Tasks');
 
 
 SELECT * FROM auth.create_app_menu_policy
 (
 	'Admin', 
 	core.get_office_id_by_office_name('Default'), 
-	'tsc',
+	'TheSmsCentral',
 	'{*}'
 );
 
--- db/SQL Server/2.x/2.0/src/03.menus/menus.sql
+++ db/SQL Server/2.x/2.0/src/03.menus/menus.sql
@@ -2,24 +2,24 @@
 WHERE menu_id IN
 (
  SELECT menu_id FROM core.menus
- WHERE app_name = 'tsc'
+ WHERE app_name = 'TheSmsCentral'
 );
 
 DELETE FROM auth.group_menu_access_policy
 WHERE menu_id IN
 (
  SELECT menu_id FROM core.menus
- WHERE app_name = 'tsc'
+ WHERE app_name = 'TheSmsCentral'
 );
 
 DELETE FROM core.menus
-WHERE app_name = 'tsc';
+WHERE app_name = 'TheSmsCentral';
 
 
-EXECUTE core.create_app 'tsc', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/tsc', NULL;
+EXECUTE core.create_app 'TheSmsCentral', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/TheSmsCentral', NULL;
 
-EXECUTE core.create_menu 'tsc', 'Tasks', 'Tasks', '', 'lightning', '';
-EXECUTE core.create_menu 'tsc', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/tsc', 'configure', 'Tasks';
+EXECUTE core.create_menu 'TheSmsCentral', 'Tasks', 'Tasks', '', 'lightning', '';
+EXECUTE core.create_menu 'TheSmsCentral', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/TheSmsCentral', 'configure', 'Tasks';
 
 GO
 DECLARE @office_id integer = core.get_office_id_by_office_name('Default');
@@ -27,7 +27,7 @@ DECLARE @office_id integer = core.get_office_id_by_office_name('Default');
 EXECUTE auth.create_app_menu_policy
 'Admin', 
 @office_id, 
-'tsc',
+'TheSmsCentral',
 '{*}'
 ;
 
