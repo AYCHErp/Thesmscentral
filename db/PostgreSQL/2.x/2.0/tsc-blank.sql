2fe378b2469f4c40c250ac4a592f42a60a29d6fe
binodnp@outlook.com <binodnp@outlook.com>
2017-06-19 01:00:14 +0545

    Routine development.

--- db/PostgreSQL/2.x/2.0/tsc-blank.sql
+++ db/PostgreSQL/2.x/2.0/tsc-blank.sql
@@ -17,10 +17,10 @@ DELETE FROM core.menus
 WHERE app_name = 'TheSmsCentral';
 
 
-SELECT * FROM core.create_app('TheSmsCentral', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/TheSmsCentral', NULL);
+SELECT * FROM core.create_app('TheSmsCentral', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/tsc', NULL);
 
 SELECT * FROM core.create_menu('TheSmsCentral', 'Tasks', 'Tasks', '', 'lightning', '');
-SELECT * FROM core.create_menu('TheSmsCentral', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/TheSmsCentral', 'configure', 'Tasks');
+SELECT * FROM core.create_menu('TheSmsCentral', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/tsc', 'configure', 'Tasks');
 
 
 SELECT * FROM auth.create_app_menu_policy
--- db/SQL Server/tsc-blank.sql
+++ db/SQL Server/tsc-blank.sql
@@ -17,10 +17,10 @@ DELETE FROM core.menus
 WHERE app_name = 'TheSmsCentral';
 
 
-EXECUTE core.create_app 'TheSmsCentral', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/TheSmsCentral', NULL;
+EXECUTE core.create_app 'TheSmsCentral', 'TheSmsCentral', 'The SMS Central', '1.0', 'MixERP Inc.', 'December 1, 2015', 'pink comments', '/dashboard/tsc', NULL;
 
 EXECUTE core.create_menu 'TheSmsCentral', 'Tasks', 'Tasks', '', 'lightning', '';
-EXECUTE core.create_menu 'TheSmsCentral', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/TheSmsCentral', 'configure', 'Tasks';
+EXECUTE core.create_menu 'TheSmsCentral', 'TheSmsCentralSetup', 'TheSmsCentral Setup', '/dashboard/tsc', 'configure', 'Tasks';
 
 GO
 DECLARE @office_id integer = core.get_office_id_by_office_name('Default');
